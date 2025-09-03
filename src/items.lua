8e88ddb77d9d5b03b654760e41607b384f754176
YUORKI <87906554+YUORKI@users.noreply.github.com>
2025-09-02 21:24:31 -0300

    Update items.lua

--- src/items.lua
+++ src/items.lua
@@ -93,5 +93,21 @@ SMODS.Consumable{
     end
 }
 
+SMODS.Consumable{
+    key = 'deathcoin', 
+    set = 'fnaf_item', 
+    atlas = 'TarotFnaf', 
+    pos = {x = 0, y = 2},
+    loc_vars = function(self, info_queue, card)
+        info_queue[#info_queue + 1] = {key = "fnaf_sprite_WIP", set = "Other"}
+        return { vars = { card.ability.extra.cards } }
+    end,
+    use = function(self, card, area, copier)
+        G.FUNCS.reroll_boss()
+    end,
+    can_use = function(self, card)
+        return G.GAME.blind.in_blind
+    end
+}
 
 
