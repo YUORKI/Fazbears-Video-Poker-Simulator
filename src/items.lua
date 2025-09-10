4fa11c1e42c1be7894d3f804389744fbbf7eba63
YUORKI <87906554+YUORKI@users.noreply.github.com>
2025-09-10 18:03:56 -0300

    Update items.lua

--- src/items.lua
+++ src/items.lua
@@ -101,9 +101,17 @@ SMODS.Consumable{
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue + 1] = {key = "fnaf_sprite_WIP", set = "Other"}        
         info_queue[#info_queue + 1] = {key = "fnaf_code_WIP", set = "Other"}
+        return { vars = { localize { type = 'name_text', set = 'Tag', key = 'tag_boss' } } }
     end,
     use = function(self, card, area, copier)
-        G.FUNCS.reroll_boss()
+        G.E_MANAGER:add_event(Event({
+            func = (function()
+                add_tag(Tag('tag_boss'))
+                play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
+                play_sound('holo1', 1.2 + math.random() * 0.1, 0.4)
+                return true
+            end)
+        }))
     end,
 }
 
